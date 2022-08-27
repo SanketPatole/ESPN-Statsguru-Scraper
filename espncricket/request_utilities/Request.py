543d7a027f64c81b2721960205dd83f0ead5b170
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2022-08-27 23:15:07 +0530

    Updated request module.

--- espncricket/request_utilities/Request.py
+++ espncricket/request_utilities/Request.py
@@ -4,17 +4,18 @@ from fake_useragent import UserAgent
 
 class Request:
     def __init__(self):
-        pass
+        self.ua = UserAgent()
+        self.formatted_url = "https://stats.espncricinfo.com/ci/engine/stats/index.html?" \
+                             "type={data_type};" \
+                             "class={match_type};" \
+                             "view={view_type};" \
+                             "template={template};" \
+                             "page={page_num}"
 
     def build_url(self, data_type, match_type, view_type, template, page_num):
-        return f"https://stats.espncricinfo.com/ci/engine/stats/index.html?" \
-               f"type={data_type};" \
-               f"class={match_type};" \
-               f"view={view_type};" \
-               f"template={template};" \
-               f"page={page_num}"
+        return self.formatted_url.format(data_type=data_type, match_type=match_type, view_type=view_type,
+                                         template=template, page_num=page_num)
 
     def get_url_object_with_agent(self, url):
-        ua = UserAgent()
-        header = {"User-Agent": str(ua.random)}
+        header = {"User-Agent": str(self.ua.random)}
         return requests.get(url, headers=header).text
