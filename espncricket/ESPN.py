5a6f5d7908254e3026eab26fe88e52a117432ccd
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2022-08-27 17:14:55 +0530

    Handled exception in get_number_of_pages function.

--- espncricket/ESPN.py
+++ espncricket/ESPN.py
@@ -7,8 +7,8 @@ class ESPN:
     def __init__(self, data_type='batting', match_type='Test', view_type=''):
         self.req_util = Request.Request()
         self.match_types = {'Test': '1', 'ODI': '2', 'T20': '3', 'All': '11',
-                       'Test_Women': '8', 'ODI_Women': '9', 'T20_Women': '10',
-                       'Test_Youth': '20', 'ODI_Youth': '21', 'T20_Youth': '22'}
+                            'Test_Women': '8', 'ODI_Women': '9', 'T20_Women': '10',
+                            'Test_Youth': '20', 'ODI_Youth': '21', 'T20_Youth': '22'}
         self.match_type = self.match_types[match_type]
         self.data_type = data_type
         self.view_type = view_type
@@ -17,14 +17,18 @@ class ESPN:
         self.list_of_dataframes = []
 
     def __get_number_of_pages(self):
-        url = self.req_util.build_url(self.data_type, self.match_type, self.view_type, self.template, page_num=1)
-        url_object = self.req_util.get_url_object_with_agent(url)
-        data = pd.read_html(url_object)
-        return int(str(data[1][0]).split("\n")[0].split(" ")[7])
+        try:
+            url = self.req_util.build_url(self.data_type, self.match_type, self.view_type, self.template, page_num=1)
+            url_object = self.req_util.get_url_object_with_agent(url)
+            data = pd.read_html(url_object)
+            return int(str(data[1][0]).split("\n")[0].split(" ")[7])
+        except:
+            return self.__get_number_of_pages()
 
     def __fetch_data(self, page_num):
         try:
-            url = self.req_util.build_url(self.data_type, self.match_type, self.view_type, self.template, page_num=page_num)
+            url = self.req_util.build_url(self.data_type, self.match_type, self.view_type, self.template,
+                                          page_num=page_num)
             url_object = self.req_util.get_url_object_with_agent(url)
             data = pd.read_html(url_object)[2]
             if self.view_type in ['series', 'ground', 'host', 'opposition']:
@@ -40,7 +44,7 @@ class ESPN:
         self.result_set = pd.DataFrame()
         number_of_pages = min(self.__get_number_of_pages(), number_of_pages)
         self.list_of_dataframes = [pd.DataFrame() for _ in range(number_of_pages)]
-        function_arguments = [page_num+1 for page_num in range(number_of_pages)]
+        function_arguments = [page_num + 1 for page_num in range(number_of_pages)]
         MultiThread.MultiThread(function=self.__fetch_data, arguments=function_arguments).run()
         result = pd.concat(self.list_of_dataframes, axis=0, ignore_index=True)
         return result[[col for col in result.columns if 'Unnamed' not in col]]
